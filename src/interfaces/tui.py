c0b2e80f90cb7625c0515cc3b92a311f029322b0
Daniel Souza <me@posix.dev.br>
2024-02-06 02:57:52 -0300

    Update chatgpt.py with strip() to remove whitespaces when checking if user is quitting
    Cherry-pick from upstream:
    https://github.com/marcolardera/chatgpt-cli/commit/f40553f0d6073ebe97102badc3c67ca13d999a7f

--- src/interfaces/tui.py
+++ src/interfaces/tui.py
@@ -160,7 +160,7 @@ def start_prompt(session: PromptSession, config: dict) -> None:
 
     message = session.prompt(HTML(f"<b>[{prompt_tokens + completion_tokens}] >>> </b>"))
 
-    if message.lower() == "/q":
+    if message.lower().strip() == "/q":
         raise EOFError
     if message.lower() == "":
         raise KeyboardInterrupt
